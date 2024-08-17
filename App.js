0eca65ae22fca04e8d387a5a35ccf856293d0d20
chaitanya-lohar <chaitanyalohar01@gmail.com>
2024-08-17 12:19:07 +0530

    Episode - 02

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
-const heading = React.createElement("div", { id: "parentDiv" }, [
-  React.createElement("div", { id: "childDiv1" }, [
-    React.createElement("h1", { id: "heading1" }, "Hello h1 tag"),
-    React.createElement("h2", { id: "heading2" }, "Hello h2 tag"),
-    React.createElement("h3", { id: "heading3" }, "Hello h3 tag"),
-  ]),
-  React.createElement("div", { id: "childDiv2" }, [
-    React.createElement("h1", { id: "heading1" }, "Hello h1 tag"),
-    React.createElement("h2", { id: "heading2" }, "Hello h2 tag"),
-    React.createElement("h3", { id: "heading3" }, "Hello h3 tag"),
-  ]),
-]);
-console.log(heading);
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("div", { id: "parentDiv" },"Namaste React🚀");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 
 
-#namaste react
+# Parcel
+- Dev Build
+- Local Server
+- HMR: Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Faster Build (By Caching)
+- Image Optimization
+- Minification
+- Compression
+- Bundling
+- Diagnostics (give clear error message)
+- Differential Bundling (support old browsers)
+- Code Splitting (This allows you to split your application code into separate bundles which can be loaded on demand)
+- Tree Shaking (remove unused code from your file)
+
