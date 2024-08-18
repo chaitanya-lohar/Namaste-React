8d95dc02771a48ce8f6b2a0e2d0ef32f66e3929d
chaitanya-lohar <chaitanyalohar01@gmail.com>
2024-08-18 20:03:30 +0530

    Upto Episode - 03

--- App.js
+++ App.js
@@ -1,7 +1,38 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", { id: "parentDiv" },"Namaste React🚀");
+// Babel transpile jsx into React.createElement
+// JSX => React.createElement(JS-Object) => HTMLElement
+
+const jsxHeading = <h1 className="heading">this is first line in jsx</h1>;
+
+// React component
+
+// Class based component - OLD
+// Functional based component - NEW
+
+const Title = () => {
+  return <h1>Title component</h1>;
+};
+
+
+const Heading = () =>(
+    <>
+    <div id="headDiv">
+    <Title/>
+    <h1>Heading Component</h1>
+    </div>
+    </>
+);
+
+const element = <span> this is element</span>;
+
+const heading = (
+    <p>this is jsx code
+     {Heading()}
+    </p>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(heading);
